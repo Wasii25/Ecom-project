55d7243f8d66a12c5c1e670a228a692b530a2e3a
Wasii25 <wasiullahrafeeq.s@gmail.com>
2025-03-01 11:46:42 +0530

    Added the POST method for new products

--- Ecom-Backend/src/main/java/com/wasi/ecombackend/service/ProductService.java
+++ Ecom-Backend/src/main/java/com/wasi/ecombackend/service/ProductService.java
@@ -3,7 +3,9 @@ package com.wasi.ecombackend.service;
 import com.wasi.ecombackend.model.Product;
 import com.wasi.ecombackend.repo.ProductRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,5 +24,12 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return repo.save(product);
     }
 }
